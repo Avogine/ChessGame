7b5a826803199ad40da9afc3fed4cb0397ecf0cc
dave <david-walk@web.de>
2023-05-09 20:18:40 +0200

    engine: pos_moves for bishop, rook & knight

--- src/engine.py
+++ src/engine.py
@@ -39,18 +39,17 @@ class Spielfeld:
             return(self.board[x][y])
         else:
             return(13)
-    def pos_move(self,x,y):
 
     def check_posmoves(self,x,y):
-        piece = self.board[x][y]
+        piece = self.return_figur(x, y)
         ret = []
         if piece == 0: # moves für keine Figur
-            return(0)
+            pass
         if piece == 1: # moves für w.Bauer
             if self.return_figur(x-1,y) == 0: # checkt 1 Feld davor
                 ret.append((x-1, y))
 
-                if self.return_figur[x-2][y] == 0: # checkt 2 Felder davor
+                if self.return_figur(x-2,y) == 0: # checkt 2 Felder davor
                     ret.append((x-2, y))
 
             if self.is_white(self.return_figur(x-1,y+1)): # checkt Feld rechts vorne auf Gegner
@@ -63,7 +62,7 @@ class Spielfeld:
             if self.return_figur(x + 1, y) == 0:  # checkt 1 Feld davor
                 ret.append((x + 1, y))
 
-                if self.return_figur[x + 2][y] == 0:  # checkt 2 Felder davor
+                if self.return_figur(x + 2, y) == 0:  # checkt 2 Felder davor
                     ret.append((x + 2, y))
 
             if self.is_white(self.return_figur(x + 1, y + 1)):  # checkt Feld rechts vorne auf Gegner
@@ -72,9 +71,53 @@ class Spielfeld:
             if self.is_white(self.return_figur(x + 1, y - 1)):  # checkt Feld links vorne auf Gegner
                 ret.append((x + 1, y - 1))
 
-        if piece == 3: # moves für knights
-            if self.return_figur(self,x, y) == 0 or self.isnt_samecolor(self,x,y):
-                ret.append((x,y))
+        if piece == 3 or piece == 4: # moves für knights
+            for i in [-2,-1,1,2]:
+                new_x = x + i
+                new_y = -abs(i) + 3
+                if self.return_figur(new_x, y - new_y) == 0 or self.isnt_samecolor(new_x, y - new_y):
+                    ret.append((new_x,y - new_y))
+
+                if self.return_figur(new_x, y + new_y) == 0 or self.isnt_samecolor(new_x,y + new_y):
+                    ret.append((new_x,y + new_y))
+
+        if piece == 5 or piece == 6: # moves for bishop
+
+            for i in [-1,1]:
+                new_x = x
+                new_y = y
+                for n in [-1,1]:
+                    new_x =+ i
+                    new_y =+ n
+                    while self.return_figur(new_x,new_y) == 0:
+                        ret.append((x+i, y+n))
+                        new_x = + i
+                        new_y = + n
+                    if self.isnt_samecolor(self.return_figur(x,y), self.return_figur(new_x, new_y)):
+                        ret.append((new_x,new_y))
+
+        if piece == 7 or piece == 8: # moves for rook
+            for i in [-1,1]:
+                new_x = x
+                new_y = y
+                while self.return_figur(new_x+i,new_y) == 0:
+                    ret.append((new_x+i,new_y))
+                    new_x =+ i
+                if self.isnt_samecolor(self.return_figur(x,y),self.return_figur(new_x+i,new_y)):
+                    ret.append((new_x+i,new_y))
+            for i in [-1,1]:
+                new_x = x
+                new_y = y
+                while self.return_figur(new_x,new_y+i) == 0:
+                    ret.append((new_x,new_y+i))
+                    new_y =+ i
+                if self.isnt_samecolor(self.return_figur(x,y),self.return_figur(new_x,new_y+i)):
+                    ret.append((new_x,new_y+i))
+
+        else:
+            pass
+        return(ret)
+
 
     def move(self,x1,y1,x2,y2):
         self.board[x2][y2] = self.board[x1][y1]
