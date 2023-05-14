7dd7e01d04a4134ce873dcc6f54a71dd5ee21eb9
Jan <jan-felix.grueninger@gmx.de>
2023-05-14 21:07:22 +0200

    Adding Pieces to background.

--- src/GUI.py
+++ src/GUI.py
@@ -1,16 +1,16 @@
 from PyQt5 import QtCore, QtWidgets, QtGui, Qt
-
+import os
 
 class GUI(QtWidgets.QApplication):
     def __init__(self):
-        super(GUI, self).__init__([])
+        super().__init__([])
         self.window = QtWidgets.QWidget()
         self.vboxlayout = QtWidgets.QVBoxLayout()  # storing the top part (board and controls) and bottom (statistics)
         self.hboxlayout = QtWidgets.QHBoxLayout()  # this second layout stores the board and controls
         self.vboxlayout.addLayout(self.hboxlayout)
 
         # add board to second layout
-        self.board = Checkerboard()
+        self.board = Board()
         self.hboxlayout.addWidget(self.board)
 
         self.window.setLayout(self.vboxlayout)
@@ -18,47 +18,9 @@ class GUI(QtWidgets.QApplication):
         self.exec()
 
 
-class Board(QtWidgets.QWidget):
-    def __init__(self, color_1=QtGui.QColor("#5f8231"), color_2=QtGui.QColor("#ffffff")):
-        super(Board, self).__init__()
-
-        # TODO: set minimum width and height for each column
-
-        # add background
-
-
-        # add draggable items to one frame
-        for column in range(0, 8):
-            for row in range(0, 8):
-                button = SquareButton()
-                button.setFlat(True)
-                button.setText(str(row) + ", " + str(column))
-                palette = button.palette()
-                palette.setColor()
-                self.addWidget(button, row, column)
-
-
-
-    print("Created GUI.")
-
-
-class SquareButton(QtWidgets.QPushButton):
-    def __init__(self, min_size=QtCore.QSize(50, 50)):
-        super(SquareButton, self).__init__()
-
-        # set size policy
-        h_policy = QtWidgets.QSizePolicy.Policy.Fixed
-        v_policy = QtWidgets.QSizePolicy.Policy.Fixed
-        policy = QtWidgets.QSizePolicy(h_policy, v_policy, QtWidgets.QSizePolicy.ControlType.PushButton)
-
-        self.setSizePolicy(policy)
-
-        self.setFixedSize(min_size)
-
-
 class Checkerboard(QtWidgets.QWidget):
-    def __init__(self, color_bright=QtGui.QColor("#ffffff"), color_dark=QtGui.QColor("#5f8231"), square_size=100):
-        super(Checkerboard, self).__init__()
+    def __init__(self, parent=None, color_bright=QtGui.QColor("#ffffff"), color_dark=QtGui.QColor("#5f8231"), square_size=100):
+        super().__init__(parent=parent)
 
         self.color_bright = color_bright
         self.color_dark = color_dark
@@ -72,7 +34,7 @@ class Checkerboard(QtWidgets.QWidget):
         # draw rects
         for row in range(0, 8):
             for column in range(0, 8):
-                is_even = ((row + column) % 2) == 0 # yes I'm very proud of this line right here...
+                is_even = ((row + column) % 2) == 0  # yes I'm very proud of this line right here...
 
                 if is_even:
                     color = self.color_dark
@@ -92,3 +54,52 @@ class Checkerboard(QtWidgets.QWidget):
 
     def sizeHint(self):
         return Qt.QSize(self.square_size * 8, self.square_size * 8)
+
+
+class Board(Qt.QWidget):
+    def __init__(self, color_1=QtGui.QColor("#5f8231"), color_2=QtGui.QColor("#ffffff")):
+        super().__init__()
+
+        # TODO: set minimum width and height for each column
+
+        # add background
+        self.checkerboard = Checkerboard(parent=self)
+        self.checkerboard.show()
+
+        # add grid layout
+        self.grid_layout = QtWidgets.QGridLayout(self)
+        self.grid_layout.setVerticalSpacing(0)
+        self.grid_layout.setHorizontalSpacing(0)
+
+        # add draggable items to one frame
+        for column in range(0, 8):
+            for row in range(0, 8):
+                sprite = QtGui.QPixmap(r'src\sprites\rook.png')
+                button = ChessPiece(sprite=sprite)
+                self.grid_layout.addWidget(button, row, column)
+
+    def minimumSizeHint(self):
+        return self.checkerboard.minimumSizeHint()
+
+    print("Created GUI.")
+
+
+class ChessPiece(QtWidgets.QLabel):
+    def __init__(self, min_size=QtCore.QSize(10, 10), sprite=None):
+        super().__init__()
+
+        # set size policy
+        h_policy = QtWidgets.QSizePolicy.Policy.Fixed
+        v_policy = QtWidgets.QSizePolicy.Policy.Fixed
+        policy = QtWidgets.QSizePolicy(h_policy, v_policy)
+
+        self.setSizePolicy(policy)
+
+        self.min_size = min_size
+
+        # set up sprite
+        self.setPixmap(sprite)
+        self.setScaledContents(True)
+
+    def sizeHint(self) -> QtCore.QSize:
+        return self.min_size
