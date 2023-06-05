17426485139ddaaa3f06c0d5a9a0a8c12981b1a0
DAVE2090 <116728708+DAVE2090@users.noreply.github.com>
2023-06-05 13:26:43 +0200

    Montag: 05.06.23 Nur moves die kein check erzeugen Castle als legaler move reverse_move

--- src/engine.py
+++ src/engine.py
@@ -12,37 +12,69 @@ def is_white(piece_num):
         return True
 
 
+def material(board):
+    ret = 0
+    for i in board:
+        if i == 0 or i == 13 or i == 11 or i == 12:
+            pass
+        elif i == 1:
+            ret += 1
+        elif i == 2:
+            ret -= 1
+        elif i == 3 or i == 5:
+            ret += 3
+        elif i == 4 or i == 6:
+            ret -= 3
+        elif i == 7:
+            ret += 5
+        elif i == 8:
+            ret -= 5
+        elif i == 9:
+            ret += 9
+        elif i == 10:
+            ret -= 9
+        else:
+            return 101
+    return ret
+
+
+standartborad = (
+    13, 8, 4, 6, 10, 12, 6, 4, 8, 13,
+    13, 2, 2, 2, 2, 2, 2, 2, 2, 13,
+    13, 0, 0, 0, 0, 0, 0, 0, 0, 13,
+    13, 0, 0, 0, 0, 0, 0, 0, 0, 13,
+    13, 0, 0, 0, 0, 0, 0, 0, 0, 13,
+    13, 0, 0, 0, 0, 0, 0, 0, 0, 13,
+    13, 1, 1, 1, 1, 1, 1, 1, 1, 13,
+    13, 7, 3, 5, 9, 11, 5, 3, 7, 13,
+)
+
+
 class Chessboard:
 
     # Do do: Enpasent, Casteln, Queen repairer
-    def __init__(self):
-        self.board = [
-            13, 8, 4, 6, 10, 12, 6, 4, 8, 13,
-            13, 2, 2, 2, 2, 2, 2, 2, 2, 13,
-            13, 0, 0, 0, 0, 0, 0, 0, 0, 13,
-            13, 0, 0, 0, 0, 0, 0, 0, 0, 13,
-            13, 0, 0, 0, 0, 0, 0, 0, 0, 13,
-            13, 0, 0, 0, 0, 0, 0, 0, 0, 13,
-            13, 1, 1, 1, 1, 1, 1, 1, 1, 13,
-            13, 7, 3, 5, 9, 11, 5, 3, 7, 13,
-        ]
+    def __init__(self, my_board=standartborad):
 
-        self.stat_check = 0
-        self.material = 0
-
+        self.board = list(my_board)     # board infos
         self.white_king_moved = True
-        self.white_king_pos = 75
-
         self.black_king_moved = True
-        self.black_king_pos = 5
-
         self.white_rrook_moved = True
         self.white_lrook_moved = True
         self.black_rrook_moved = True
         self.black_lrook_moved = True
+
+        self.speicher = []
+
+        self.stat_check = 0
+        self.material = material(self.board)
+
+        self.black_king_pos = 5
+        self.white_king_pos = 75
+
+
         self.movecount = 1
 
-    def isnt_samecolor(self, piece_num, other_piece_num):
+    def isnt_samecolor(self, piece_num, other_piece_num):   # gib zwei Felder & wenn !gleichfarbige Figuren return "True"
         if self.return_figur(other_piece_num) != 13 and self.return_figur(other_piece_num) != 0:
             if self.return_figur(piece_num) % 2 != self.return_figur(other_piece_num) % 2:
                 return True
@@ -51,7 +83,7 @@ class Chessboard:
         else:
             return False
 
-    def rocharden_check(self, pos):
+    def rocharden_check(self, pos):    # änder Werte falls nötig
         if pos == 1:
             self.black_lrook_moved = False
         elif pos == 8:
@@ -67,124 +99,235 @@ class Chessboard:
         else:
             pass
 
-    def check_search(self, pos, color):
+    def return_figur(self, pos, my_board=()):
+        if not my_board:
+            my_board = self.board
+        if 0 <= pos <= 79:
+            return my_board[pos]
+        else:
+            return 13
+
+    def check_search(self, pos, color, my_board=()):
         ret = False
+        if not my_board:
+            my_board = self.board
         if color:  # white king
             for i in [-11, -9, 9, 11]:  # bishop
                 new_pos = pos + i
-                while self.return_figur(new_pos) == 0:
+                while self.return_figur(new_pos, my_board) == 0:
                     new_pos += i
-                if self.return_figur(new_pos) == 6 or self.return_figur(new_pos) == 10:
+                if self.return_figur(new_pos, my_board) == 6 or self.return_figur(new_pos, my_board) == 10:
                     ret = True
 
             if not ret:  # knight
                 for i in [-21, -19, -12, -8, 8, 12, 19, 21]:
                     new_pos = pos + i
-                    if self.return_figur(new_pos) == 4:
+                    if self.return_figur(new_pos, my_board) == 4:
                         ret = True
 
             if not ret:  # rook
                 for i in [-10, -1, 1, 10]:
                     new_pos = pos + i
-                    while self.return_figur(new_pos) == 0:
+                    while self.return_figur(new_pos, my_board) == 0:
                         new_pos += i
-                    if self.return_figur(new_pos) == 8 or self.return_figur(new_pos) == 10:
+                    if self.return_figur(new_pos, my_board) == 8 or self.return_figur(new_pos, my_board) == 10:
                         ret = True
 
             if not ret:  # pwan
-                if self.return_figur(pos - 9) == 2 or self.return_figur(pos - 11) == 2:
+                if self.return_figur(pos - 9, my_board) == 2 or self.return_figur(pos - 11, my_board) == 2:
                     ret = True
         else:
             for i in [-11, -9, 9, 11]:  # bishop
                 new_pos = pos + i
-                while self.return_figur(new_pos) == 0:
+                while self.return_figur(new_pos, my_board) == 0:
                     new_pos += i
-                if self.return_figur(new_pos) == 5 or self.return_figur(new_pos) == 9:
+                if self.return_figur(new_pos, my_board) == 5 or self.return_figur(new_pos, my_board) == 9:
                     ret = True
 
             if not ret:  # knight
                 for i in [-21, -19, -12, -8, 8, 12, 19, 21]:
                     new_pos = pos + i
-                    if self.return_figur(new_pos) == 3:
+                    if self.return_figur(new_pos, my_board) == 3:
                         ret = True
 
             if not ret:  # rook
                 for i in [-10, -1, 1, 10]:
                     new_pos = pos + i
-                    while self.return_figur(new_pos) == 0:
+                    while self.return_figur(new_pos, my_board) == 0:
                         new_pos += i
-                    if self.return_figur(new_pos) == 7 or self.return_figur(new_pos) == 9:
+                    if self.return_figur(new_pos, my_board) == 7 or self.return_figur(new_pos, my_board) == 9:
                         ret = True
 
-            if not ret:  # pwan
-                if self.return_figur(pos + 9) == 1 or self.return_figur(pos + 11) == 1:
+            if not ret:  # pawn
+                if self.return_figur(pos + 9, my_board) == 1 or self.return_figur(pos + 11, my_board) == 1:
                     ret = True
 
         return ret
 
     def move(self, pos, new_pos):
-        self.movecount += 1
-        taken = self.return_figur(new_pos)
-        self.board[new_pos] = self.board[pos]
-        self.board[pos] = 0
+        self.movecount += 1  # movecount erhöhen
+        taken = self.return_figur(new_pos)  # figur auf dem Feld das betreten wurde
 
-        self.rocharden_check(pos)
+        self.board[new_pos] = self.board[pos]  # neue Feld ist gleich der Figur
+        self.board[pos] = 0  # Feld der Figur ist jetzt frei(=0)
 
-        if self.movecount % 2 == 0:
-            if pos == self.white_king_pos:
-                self.white_king_pos = new_pos
-            if self.check_search(self.black_king_pos, False):
-                self.stat_check = 1
-                print("[T] Black king in check")
-            else:
-                self.stat_check = 0
+        self.rocharden_check(pos)  # sollte ein Rook oder ein King bewegt worden sein wird es gespeichert
+
+        if pos == self.white_king_pos:
+            self.white_king_pos = new_pos  # um leichter nach checks zu suchen wird die pos des Kings gespeichert
+        if self.check_search(self.black_king_pos, False):
+            self.stat_check = 1   # Black in check
         else:
-            if pos == self.black_king_pos:
-                self.black_king_pos = new_pos
-            if self.check_search(self.white_king_pos, True):
-                self.stat_check = 2
-                print(f"[T] White king in check")
-            else:
-                self.stat_check = 0
+            self.stat_check = 0
+
+        if pos == self.black_king_pos:
+            self.black_king_pos = new_pos
+        if self.check_search(self.white_king_pos, True):
+            self.stat_check = 2    # White in check
+
+
         if taken != 0:
             if taken == 1:
                 self.material -= 1
-                return "Pawn"
             elif taken == 2:
                 self.material += 1
-                return "Pawn"
             elif taken == 3:
                 self.material -= 3
-                return "Knight"
             elif taken == 4:
                 self.material += 3
-                return "Knight"
             elif taken == 5:
                 self.material -= 3
-                return "Bishop"
             elif taken == 6:
                 self.material += 3
-                return "Bishop"
             elif taken == 7:
                 self.material -= 5
-                return "Rook"
             elif taken == 8:
                 self.material += 5
-                return "Rook"
             elif taken == 9:
                 self.material -= 9
-                return "Queen"
             elif taken == 10:
                 self.material += 9
-                return "Queen"
         else:
-            return ""
+            taken = 0
 
-    def return_figur(self, pos):
-        if 0 <= pos <= 79:
-            return self.board[pos]
+            # datei = pos, new_pos, taken, castlen_white, castlen_black
+            # rocharde:
+            #    0 = niemand
+            #    1 = nur rechts
+            #    2 = nur links
+            #    3 = alle
+
+        move_datei = [pos, new_pos, taken]
+        white_cas = 0
+        if self.white_king_moved:
+            if self.white_rrook_moved and self.white_lrook_moved:
+                white_cas = 3
+            elif self.white_rrook_moved:
+                white_cas = 1
+            elif self.white_lrook_moved:
+                white_cas = 2
+        black_cas = 0
+        if self.black_king_moved:
+            if self.black_rrook_moved and self.black_lrook_moved:
+                black_cas = 3
+            elif self.black_rrook_moved:
+                white_cas = 1
+            elif self.black_lrook_moved:
+                white_cas = 2
+        if white_cas or black_cas:
+            move_datei.append(white_cas)
+            move_datei.append(black_cas)
+
+        self.speicher.append(move_datei)
+
+
+    def reverse_move(self,my_board=()):
+        if not my_board:
+            my_board = self.board
+        datei = self.speicher.pop(self.movecount - 2)
+        w_cas = 0
+        b_cas = 0
+        if len(datei) == 3:
+            old_pos, now_pos, taken = datei
         else:
-            return 13
+            old_pos, now_pos, taken, w_cas, b_cas = datei
+        print(old_pos, now_pos, taken, w_cas, b_cas)
+        self.movecount -= 1  # movecount verringert
+
+        if w_cas:
+            self.white_king_moved = True
+            if w_cas == 1:
+                self.white_rrook_moved = True
+                self.white_lrook_moved = False
+            elif w_cas == 2:
+                self.white_rrook_moved = False
+                self.white_lrook_moved = True
+            elif w_cas == 3:
+                self.white_rrook_moved = True
+                self.white_lrook_moved = True
+            else:
+                print("Fehler 354")
+        else:
+            self.white_king_moved = False
+            self.white_rrook_moved = False
+            self.white_lrook_moved = False
+
+        if b_cas:
+            self.black_king_moved = True
+            if b_cas == 1:
+                self.black_rrook_moved = True
+                self.black_lrook_moved = False
+            elif b_cas == 2:
+                self.black_rrook_moved = False
+                self.black_lrook_moved = True
+            elif b_cas == 3:
+                self.black_rrook_moved = True
+                self.black_lrook_moved = True
+            else:
+                print("Fehler 353")
+        else:
+            self.black_king_moved = False
+            self.black_rrook_moved = False
+            self.black_lrook_moved = False
+
+        my_board[old_pos] = my_board[now_pos]  # altes Feld ist gleich der Figur
+        my_board[now_pos] = taken
+
+        if taken != 0:
+            if taken == 1:
+                self.material += 1
+            elif taken == 2:
+                self.material -= 1
+            elif taken == 3:
+                self.material += 3
+            elif taken == 4:
+                self.material -= 3
+            elif taken == 5:
+                self.material += 3
+            elif taken == 6:
+                self.material -= 3
+            elif taken == 7:
+                self.material += 5
+            elif taken == 8:
+                self.material -= 5
+            elif taken == 9:
+                self.material += 9
+            elif taken == 10:
+                self.material -= 9
+
+        if now_pos == self.white_king_pos:
+            self.white_king_pos = old_pos  # um leichter nach checks zu suchen wird die pos des Kings gespeichert
+        if self.check_search(self.black_king_pos, False):
+            self.stat_check = 1
+        else:
+            self.stat_check = 0
+
+        if now_pos == self.black_king_pos:
+            self.black_king_pos = old_pos
+        if self.check_search(self.white_king_pos, True):
+            self.stat_check = 2
+            # hab das zweite stat_check = 0 weg gelassen, weil sonst die 1 überschrieben werden könnten.
+            # diese Lösung könnte allerdings in Zukunft zu Fehlern führen
 
     def check_pos_moves(self, pos):
         piece_num = self.return_figur(pos)
@@ -270,12 +413,15 @@ class Chessboard:
                                                                           self.return_figur(new_pos)):
                     ret.append(new_pos)
 
-            if self.white_king_moved:  # rocharde
+            if self.white_king_moved and self.stat_check == 0:  # castlen
                 if self.white_rrook_moved and self.return_figur(76) + self.return_figur(77) == 0:
-                    ret.append(78)
+                    if not self.check_search(76, True) and not self.check_search(77, True):
+                        ret.append(77)
                 if self.white_lrook_moved and self.return_figur(74) + self.return_figur(73) + self.return_figur(
                         72) == 0:
-                    ret.append(71)
+                    if not self.check_search(72, True) and not self.check_search(73, True) and not self.check_search(74, True):
+                        ret.append(73)
+
         elif piece_num == 12:  # moves for the black king
             for i in [1, 9, 10, 11, -11, -10, -9, -1]:
                 new_pos = pos + i
@@ -285,4 +431,25 @@ class Chessboard:
 
         else:
             pass
+
+        if self.movecount%2 == 0:  # nur moves die Check stoppen
+            new_ret = []
+            for i in ret:
+                self.move(pos, i)
+                print(i)
+                if self.stat_check != 2:
+                    new_ret.append(i)
+                    print(self.stat_check)
+                self.reverse_move()
+            print(new_ret)
+            ret = new_ret
+
+        else:
+            new_ret = []
+            for i in ret:
+                self.move(pos, i)
+                if self.stat_check != 1:
+                    new_ret.append(i)
+                self.reverse_move()
+            ret = new_ret
         return ret
